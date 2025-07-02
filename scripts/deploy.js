06a95c9f158eed2c8ded11dba406c78253cc6f0d
Daniel235-web <samueledaniel235@gmail.com>
2025-07-02 16:59:53 +0100

    modified the deploy scriptto deply 3 smart contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,44 @@
-const {ethers, artifacts} = require("hardhat");
+const { ethers, artifacts } = require("hardhat");
 const fs = require("fs");
-async function main()  {
-    const [deployer] =  await ethers.getSigners();
-    const tokenContractFactory = await ethers.getContractFactory("SimpleDeFiToken");
-    const token  = await tokenContractFactory.deploy();
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const contractList = [
+    // "Contract Name ", "Contract Factory Name"
+    ["Simple DeFi Token", "SimpleDeFiToken"],
+    ["Meme Token", "MemeToken"],
+    ["Pair Factory", "PairFactory"]
 
-    console.log("Simple DeFi token contract address: ", token.address);
-    console.log("Deployer's address", deployer.address);
-    console.log("Deployer's ETH balance", (await deployer.provider.getBalance(deployer.address)).toString());
-   
-   saveContractToFrontend(token, 'SimpleDeFiToken');
+  ];
 
+  //Deploying the smart contracts and safe  the contract to frontend
+ 
+  for (const [name, factory] of contractList) {
+    let contractFactory = await ethers.getContractFactory(factory);
+    let contract = await contractFactory.deploy()
+    console.log(`${name} Contract Address:`, contract.address);
+    saveContractToFrontend(contract, factory);
+  }
+
+
+  console.log("Deployer's address", deployer.address);
+  console.log("Deployer's ETH balance", (await deployer.provider.getBalance(deployer.address)).toString());
 }
 function saveContractToFrontend(contract, name) {
-    const contractDir = __dirname + "/../src/frontend/contracts";    
- 
-    if (!fs.existsSync(contractDir)){
-        fs.mkdirSync(contractDir);
-    }
-    fs.writeFileSync(
-        contractDir + `/${name}-address.json`,
-        JSON.stringify({address: contract.address}, undefined, 2)
-    );
-    const contractArtifacts = artifacts.readArtifactSync(name);
-    
-    fs.writeFileSync(
-        contractDir + `/${name}.json`, JSON.stringify(contractArtifacts, null, 2)
-    )
+  const contractDir = __dirname + "/../src/frontend/contracts";
+
+  if (!fs.existsSync(contractDir)) {
+    fs.mkdirSync(contractDir);
+  }
+  fs.writeFileSync(
+    contractDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+  const contractArtifacts = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(contractDir + `/${name}.json`, JSON.stringify(contractArtifacts, null, 2));
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitcode = 1;
+  console.error(error);
+  process.exitcode = 1;
 });
