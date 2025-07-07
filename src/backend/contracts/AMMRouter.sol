0121029ad88e5face45939d57d4d7ae559245acd
Daniel235-web <samueledaniel235@gmail.com>
2025-07-07 08:53:18 +0100

    AMM router is a smart contract for users and off-chain systems to access the DEX

---
+++ src/backend/contracts/AMMRouter.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Indentifier: MIT
+pragma solidity ^0.8.0;
+import "./interfaces/IPairFactory.sol";
+import "./interfaces/IAMMRouter.sol";
+import "./libraries/Helper.sol";
+import "./interfaces/ITokenPair.sol";
+
+
+contract AMMRouter  is IAMMRouter {
+    address public immutable factory;
+    bytes32 private initCodeHash;
+    constructor(address _factory) {
+        factory = _factory;
+        initCodeHash  = IPairFactory(factory).INIT_CODE_PAIR_HASH();
+
+    }
+
+    modifier ensure (uint256 deadline) {
+        require (deadline >= block.timestamp, "EXPIRED");
+        _;
+
+    }
+    //Fetch reserves and pair address of a pair while respecting the token  order
+    function getReserves(address tokenA, address tokenB)
+    public
+    view 
+    returns(uint256 reserveA, uint256 reserveB, address pair){
+        (address _tokenA, )  = Helper.sortTokens(tokenA, tokenB);
+        pair = Helper.pairFor(factory, tokenA, tokenB, initCodeHash);
+        (uint256 _reserveA, uint256 _reserveB, ) = ITokenPair(pair).getReserves();
+        (reserveA, reserveB) = tokenA ==_tokenA ? (_reserveA, _reserveB) : (_reserveB, _reserveA);
+    }
+//perform getAmount calculations along the pair in the path
+function getAmountOut(uint256 amountIn, address[] memory path) public view returns (uint256[] memory amounts){
+    require (path.length >=2, "INVALID_PATH");
+    amounts = new uint256[](path.length);
+    amounts[0] = amountIn;
+    for (uint256 i; i < path.length -1 ; i++) {
+        (uint256 reserveIn, uint256 reserveOut, ) = getReserves(
+            path[i],
+            path[i + 1]
+        );
+        amounts[i + 1 ] = Helper.getAmountOut(amounts[i], reserveIn, reserveOut);
+    }
+}
+}
