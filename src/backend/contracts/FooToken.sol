caa58416c2b7c6559c436da3f30fde7ac991c295
Daniel235-web <samueledaniel235@gmail.com>
2025-08-10 02:55:07 +0100

    added another memecoin called Bar token with an initial supply of 1 million to the deployer

---
+++ src/backend/contracts/FooToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract FooToken is ERC20 {
+    constructor() ERC20("Foo Token", "FOO") {
+        // Initial supply of 1,000,000,000 tokens are given to msg.sender
+        _mint(msg.sender, 1e27);
+    }
+}
