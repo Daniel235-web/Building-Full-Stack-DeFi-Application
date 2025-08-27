6a778c59ad95a080cc647aac6861aa9350d1f184
Daniel235-web <samueledaniel235@gmail.com>
2025-08-27 17:26:20 +0100

    modified

--- src/frontend/components/TokenSelectModal/index.js
+++ src/frontend/components/TokenSelectModal/index.js
@@ -1,55 +1,68 @@
-import { useState, useCallback, useEffect } from "react";
-import { SupportedTokens } from "../../utils/Tokens";
-import { getTokenInfo } from "../../utils/Helpers";
-import { Dialog, IconButton, DialogTitle, DialogContent, List, ListItem, Typography } from "@mui/material";
+import { useState, useEffect, useCallback } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
-import WETH from "../../contracts/WETH-address.json";
+import { SupportedTokens } from "../../utils/Tokens";
+import { getTokenInfo } from '../../utils/Helpers';
+import WETH from '../../contracts/WETH-address.json'
 
-const TokenSelectModal = ({ open, handleClose, selectToken }) => {
+const TokenSelectModal = ({ open, handleClose, selectToken, erc20Only, customTokens }) => {
   const [tokens, setTokens] = useState([]);
+
   const getSupportedTokens = useCallback(async () => {
-    const _tokens = [
-      { address: WETH.address, name: "Ether", symbol: "ETH", decimals: 18 },
-      { address: WETH.address, name: "Wrapped ETH", symbol: "WETH", decimals: 18 },
-    ];
+    if (customTokens && customTokens.length > 0) {
+      setTokens(customTokens);
+      return;
+    }
+    // The native coin of EVM and its wrapped form
+    const _tokens = [{
+      address: WETH.address,
+      name: 'Ether',
+      symbol: 'ETH',
+      decimals: 18
+    }, {
+      address: WETH.address,
+      name: 'Wrapped ETH',
+      symbol: 'WETH',
+      decimals: 18
+    }];
+    if (erc20Only) {
+      // Remove the first element since ETH is not an ERC20 token
+      _tokens.shift();
+    }
     for (let address of SupportedTokens) {
       _tokens.push(await getTokenInfo(address));
     }
     setTokens(_tokens);
-  }, []);
+  }, [erc20Only, customTokens]);
 
   useEffect(() => {
     getSupportedTokens();
-  }, [getSupportedTokens]);
-
-  return (
-    <Dialog open={open} onClose={handleClose}>
-      <IconButton>
-        <ClearOutlinedIcon />
-      </IconButton>
-      <DialogTitle>
-        <Typography>Please select a token</Typography>
-      </DialogTitle>
-      <DialogContent>
-        <List>
-          {tokens.map((item, index) => (
-            <ListItem
-              key={index}
-              sx={{ "&: hover": { background: "#1976d2", cursor: "pointer" } }}
-              onClick={() => {
-                handleClose();
-                selectToken(item);
-              }}
-            >
-              <Typography>
-                {item.name} ({item.symbol})
-              </Typography>
-            </ListItem>
-          ))}
-        </List>
-      </DialogContent>
-    </Dialog>
-  );
-};
+  }, [getSupportedTokens])
+  return <Dialog open={open} onClose={handleClose}>
+    <IconButton onClick={handleClose}><ClearOutlinedIcon /></IconButton>
+    <DialogTitle>
+      <Typography>Please select a token</Typography>
+    </DialogTitle>
+    <DialogContent>
+      <List>
+        {tokens.map((item, index) =>
+          <ListItem key={index}
+            sx={{ "&: hover": { background: "#1976d2", cursor: "pointer" } }}
+            onClick={() => { handleClose(); selectToken(item); }}>
+            <Typography>{item.name} ({item.symbol})</Typography>
+          </ListItem>)
+        }
+      </List>
+    </DialogContent>
+  </Dialog>
+}
 
 export default TokenSelectModal;
--- src/frontend/utils/Helpers.js
+++ src/frontend/utils/Helpers.js
@@ -1,13 +1,14 @@
-import { localProvider } from "../components/wallet";
-import { ethers } from "ethers";
-import { ERC20ABI } from "./ERC20ABI";
+import { localProvider } from '../components/wallet';
+import { ethers } from 'ethers';
+import { ERC20ABI } from './ERC20ABI';
+import FactoryABI from '../contracts/PairFactory.json';
+import FactoryAddress from '../contracts/PairFactory-address.json';
+import { TokenPairABI } from './TokenPairABI';
 import WETH from '../contracts/WETH-address.json';
 
 export const getTokenInfo = async (address) => {
-  let name = "Unknown",
-    symbol = "Unknown",
-    decimals = 18;
-      if (address === WETH.address) {
+  let name = "Unknown", symbol = "Unknown", decimals = 18;
+  if (address === WETH.address) {
     // Shortcut for Ether
     return { address, name: "Ether", symbol: "ETH", decimals: 18 };
   }
@@ -20,31 +21,53 @@ export const getTokenInfo = async (address) => {
     console.error(error);
   }
   return { address, name, symbol, decimals };
-};
+}
+
+export const getLiquidityPools = async () => {
+  const pools = new Map();
+  try {
+    const factory = new ethers.Contract(FactoryAddress.address, FactoryABI.abi, localProvider);
+    const nPairs = await factory.allPairsLength();
+    for (let i = 0; i < nPairs; i++) {
+      const address = await factory.allPairs(i);
+      const tokenPair = new ethers.Contract(address, TokenPairABI, localProvider);
+      const tokenA = await getTokenInfo(await tokenPair.tokenA());
+      const tokenB = await getTokenInfo(await tokenPair.tokenB());
+      pools.set(address, { tokenA, tokenB });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  return pools;
+}
+
 export const ERROR_CODE = {
-  ACTION_REJECTED: "Action rejected by user!",
-};
+  'ACTION_REJECTED': 'Action rejected by user!'
+}
 
 export const getErrorMessage = (error, defaultMessage) => {
   return ERROR_CODE[error.code] || defaultMessage;
-};
-export const toString = (x) => {
+}
+
+export const toString = x => {
   if (Math.abs(x) < 1.0) {
-    let e = parseInt(x.toString().split("e-")[1]);
+    let e = parseInt(x.toString().split('e-')[1]);
     if (e) {
       x *= Math.pow(10, e - 1);
-      x = "0." + new Array(e).join("0") + x.toString().substring(2);
+      x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
     }
   } else {
-    let e = parseInt(x.toString().split("+")[1]);
+    let e = parseInt(x.toString().split('+')[1]);
     if (e > 20) {
       e -= 20;
       x /= Math.pow(10, e);
-      x += new Array(e + 1).join("0");
+      x += (new Array(e + 1)).join('0');
     }
   }
   return x.toString();
-};
+}
+
+// Check if a token object is ETH
 export const isETH = token => {
   return token.address === WETH.address && token.symbol === 'ETH';
 }
